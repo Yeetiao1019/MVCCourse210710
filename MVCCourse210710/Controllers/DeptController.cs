403f545ff6aa5f0bd5ab87fd47b5eafe63b3c4dd
Yeetiao Chen <n1533512@gmail.com>
2021-07-11 11:53:52 +0800

    新增DeptController CRUD功能與ViewModel

--- MVCCourse210710/Controllers/DeptController.cs
+++ MVCCourse210710/Controllers/DeptController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
+using MVCCourse210710.ViewModels;
+using System.Net;
 
 namespace MVCCourse210710.Controllers
 {
@@ -20,6 +22,122 @@ namespace MVCCourse210710.Controllers
         public ActionResult Index()
         {
             return View(db.Department.Include(d => d.Manager));
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "LastName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DepartmentCreate departmentCreate)
+        {
+            if (ModelState.IsValid)
+            {
+                Department department = new Department();
+                department.Name = departmentCreate.Name;
+                department.Budget = departmentCreate.Budget;
+                department.InstructorID = departmentCreate.InstructorID;
+                department.StartDate = DateTime.Now;
+
+                db.Department.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "LastName", departmentCreate.InstructorID);
+            return View(departmentCreate);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+        // GET: Departments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+
+            DepartmentEdit departmentEdit = new DepartmentEdit();
+            departmentEdit.DepartmentID = department.DepartmentID;
+            departmentEdit.Name = department.Name;
+            departmentEdit.Budget = department.Budget;
+            departmentEdit.InstructorID = department.InstructorID;            
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "LastName", departmentEdit.InstructorID);
+            return View(departmentEdit);
+        }
+
+        // POST: Departments/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int? id, DepartmentEdit departmentEdit)
+        {
+            Department department = new Department();
+            if (ModelState.IsValid)
+            {
+                department = db.Department.Find(id);
+                department.DepartmentID = departmentEdit.DepartmentID;
+                department.Name = departmentEdit.Name;
+                department.Budget = departmentEdit.Budget;
+                department.InstructorID = departmentEdit.InstructorID;                
+
+                db.Department.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "LastName", department.InstructorID);
+            return View(departmentEdit);
+        }
+
+        // GET: Departments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Department.Find(id);            
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            DepartmentDelete departmentDelete = new DepartmentDelete();
+            departmentDelete.Name = department.Name;
+            return View(departmentDelete);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id , DepartmentDelete departmentDelete)
+        {
+            Department department = db.Department.Find(id);
+            departmentDelete.Name = department.Name;
+
+            db.Department.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ MVCCourse210710/ViewModels/DepartmentDelete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCourse210710.ViewModels
+{
+    public class DepartmentDelete
+    {
+        public string Name { get; set; }
+    }
+}
