1140e944385b241970295ec4c6ec8dfed43111bd
Yeetiao Chen <n1533512@gmail.com>
2021-07-17 16:12:24 +0800

    各種從Action傳值到View的方式

---
+++ MVCCourse210710/Controllers/MBController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCourse210710.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MB1()
+        {
+            var data = "Hello World";
+            ViewData.Model = data;
+            return View(data);
+        }
+        public ActionResult MB2()
+        {
+            var data = "Hello World";
+            ViewData["data"]= data;
+            return View("MB1");
+        }
+        public ActionResult MB3()
+        {
+            ViewBag.data = "Hello World";
+            return View("MB1");
+        }
+        public ActionResult MB4()
+        {
+            TempData["data"] = "Hello World";
+            return RedirectToAction("MB2");
+        }
+        public ActionResult MB5()
+        {
+            Session["SessionData"] = "Hello World";
+            return View("MB1");
+        }
+    }
+}
