08d0aa2b83f50afed81bbb7ddd2567307a4e94f5
陳沂 <polochen@ytsys.com>
2021-07-24 15:43:07 +0800

    加入MVC的新增Controller、View的範例檔，並在範本中加入X.PagedList的分頁功能，以此範本新增PersonTempController

---
+++ MVCCourse210710/Controllers/PeopleTempController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using X.PagedList;
+using MVCCourse210710.Models;
+
+namespace MVCCourse210710.Controllers
+{
+    public class PeopleTempController : Controller
+    {
+        private ContosoUniversityEntities db = new ContosoUniversityEntities();
+
+        // GET: PeopleTemp
+        public ActionResult Index(int pageNum = 1 , int pageSize = 2)
+        {
+            var person = db.Person.Include(p => p.OfficeAssignment).OrderBy(o => o.ID);
+            return View(person.ToPagedList(pageNum,pageSize));
+        }
+
+        // GET: PeopleTemp/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: PeopleTemp/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID = new SelectList(db.OfficeAssignment, "InstructorID", "Location");
+            return View();
+        }
+
+        // POST: PeopleTemp/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,LastName,FirstName,HireDate,EnrollmentDate,Discriminator")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID = new SelectList(db.OfficeAssignment, "InstructorID", "Location", person.ID);
+            return View(person);
+        }
+
+        // GET: PeopleTemp/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignment, "InstructorID", "Location", person.ID);
+            return View(person);
+        }
+
+        // POST: PeopleTemp/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,HireDate,EnrollmentDate,Discriminator")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignment, "InstructorID", "Location", person.ID);
+            return View(person);
+        }
+
+        // GET: PeopleTemp/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: PeopleTemp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Person person = db.Person.Find(id);
+            db.Person.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
